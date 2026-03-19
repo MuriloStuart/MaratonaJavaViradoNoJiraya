2578c55ced881a09fe00b4c900cf7ce6f5e48776
Murilo Stuart <stuartmurilo10@gmail.com>
2026-03-18 22:26:22 -0300

    Aulas 144 e 145

---
+++ src/projectsidea/devdojo/maratonajava/java/core/Wnio/test/PathTest01.java
@@ -0,0 +1,17 @@
+package projectsidea.devdojo.maratonajava.java.core.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("C:\\Users\\stuar\\Downloads\\Backup\\primeiroprograma\\File.txt");
+        Path p2 = Paths.get("C:\\Users\\stuar\\Downloads\\Backup\\primeiroprograma","file.txt");
+        Path p3 = Paths.get("C:","\\Users\\stuar\\Downloads\\Backup\\primeiroprograma\\File.txt");
+        Path p4 = Paths.get("C:","Users","stuar","Downloads","Backup","primeiroprograma","File.txt");
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+    }
+}
