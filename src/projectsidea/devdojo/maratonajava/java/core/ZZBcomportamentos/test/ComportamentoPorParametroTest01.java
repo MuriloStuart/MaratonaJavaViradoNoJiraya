c805fafd4177dd666e596e62061f2aae64136345
Murilo Stuart <stuartmurilo10@gmail.com>
2026-04-16 20:26:36 -0300

    Aula 193

---
+++ src/projectsidea/devdojo/maratonajava/java/core/ZZBcomportamentos/test/ComportamentoPorParametroTest01.java
@@ -0,0 +1,66 @@
+package projectsidea.devdojo.maratonajava.java.core.ZZBcomportamentos.test;
+
+import projectsidea.devdojo.maratonajava.java.core.ZZBcomportamentos.dominio.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComportamentoPorParametroTest01 {
+
+   private  static List<Car> cars = List.of(
+            new Car("green", 2011),
+            new Car("black", 1998),
+            new Car("red", 2019)
+    );
+
+
+    public static void main(String[] args) {
+        System.out.println(filterGreenCar(cars));
+        System.out.println(filterRedCar(cars));
+        System.out.println(filterByColor(cars,"green"));
+        System.out.println(filterByColor(cars,"red"));
+        System.out.println("--------");
+        System.out.println(filterByAgeBefore(cars,2015));
+    }
+
+    private static List<Car> filterGreenCar(List<Car> cars) {
+        List<Car> greenCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getColor().equals("green")) {
+                greenCars.add(car);
+            }
+        }
+        return greenCars;
+    }
+
+
+    private static List<Car> filterRedCar(List<Car> cars) {
+        List<Car> greenCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getColor().equals("red")) {
+                greenCars.add(car);
+            }
+        }
+        return greenCars;
+    }
+
+    private static List<Car> filterByColor(List<Car> cars, String color) {
+        List<Car> filteredCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getColor().equals(color)) {
+                filteredCars.add(car);
+            }
+        }
+        return filteredCars;
+    }
+
+    private static List<Car> filterByAgeBefore(List<Car> cars, int year) {
+        List<Car> filteredCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getYear() < year) {
+                filteredCars.add(car);
+            }
+        }
+        return filteredCars;
+    }
+}
